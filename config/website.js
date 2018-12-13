357341aacc0e016c9365ddbcb9593476b0b03252
maysssam@gmail.com <maysssam@gmail.com>
2018-12-13 10:45:24 -0800

    ana

--- config/website.js
+++ config/website.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  siteTitle: 'Zimarc', // Navigation and Site Title
-  siteTitleAlt: 'Zimarc - Collaborative Design Studio', // Alternative Site title for SEO
-  siteUrl: 'https://zimarc.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Maysam', // Navigation and Site Title
+  siteTitleAlt: 'Maysam Architecture Design Portfolio ', // Alternative Site title for SEO
+  siteUrl: 'https://maysam.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Architectural Design Portfolio by Zimarc Team',
+  siteDescription: 'Architectural Design Portfolio by Maysam',
   author: 'Maysam', // Author for schemaORGJSONLD
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@Zimarcdotcom', // Twitter Username
   ogSiteName: 'zimarcpage', // Facebook Site Name
   ogLanguage: 'en_US',
-  googleAnalyticsID: 'UA-129890924-1',
+  googleAnalyticsID: 'UA-130241395-1',
   // Manifest and Progress color
   themeColor: '#3498DB',
   backgroundColor: '#bada55',
